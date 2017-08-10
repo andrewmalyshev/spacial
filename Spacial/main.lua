05cfa6d268983148bd216537760d53945e475796
Andrew Malyshev <andriy.malyshev@gmail.com>
2017-08-10 18:37:54 +0300

    Added and display resources
    Added and display temporary ship and background resources

--- Spacial/main.lua
+++ Spacial/main.lua
@@ -4,4 +4,10 @@
 --
 -----------------------------------------------------------------------------------------
 
--- Your code here
+
+local background = display.newImageRect( "spaceBackground.png", 1080, 1920)
+
+
+local ship = display.newImageRect( "spaceShip.png", 70, 140)
+ship.x = display.contentCenterX
+ship.y = display.contentCenterY
